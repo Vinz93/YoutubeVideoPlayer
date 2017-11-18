55dcaffdb0c07bffe7caa0cea3b5c2d3f0c120eb
Vincenzo Bianco <vincenzo.bianco@wikot.com>
2017-11-18 16:16:52 -0400

    first commit

--- src/components/app.js
+++ src/components/app.js
@@ -3,7 +3,15 @@ import React, { Component } from 'react';
 export default class App extends Component {
   render() {
     return (
-      <div>React simple starter</div>
+      <div>
+        <h1>The componnent</h1>
+        <div>
+          <h2>Hey {this.props.name}</h2>
+          <ul>
+            { [1,2,3].map(e => <li key={e} >map elements {e * 2}</li>) };
+          </ul>
+        </div>
+      </div>
     );
   }
 }
--- src/index.js
+++ src/index.js
@@ -1,15 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
 
 import App from './components/app';
-import reducers from './reducers';
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
 
-ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
-  </Provider>
-  , document.querySelector('.container'));
+ReactDOM.render(<div>
+  <App name='Jhon'/>
+  <App name='Vincenzo' />
+  <App name='Barbara'/>
+</div>, document.querySelector('.container'));
